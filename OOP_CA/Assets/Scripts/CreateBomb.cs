df5a8bed4fc48e2b1b1a28a5062f323af09e15bf
Nafeesa <nafyxnaz@gmail.com>
2018-04-24 17:40:47 +0100

    commit
    commit

---
+++ OOP_CA/Assets/Scripts/CreateBomb.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class CreateBomb : MonoBehaviour 
+{
+	public Tilemap tilemap;
+
+	public GameObject bombPrefab;
+
+		
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			Vector3 worldPos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			Vector3Int cell = tilemap.WorldToCell(worldPos);
+			Vector3 cellCenterPros = tilemap.GetCellCenterWorld(cell);
+
+			Instantiate(bombPrefab, cellCenterPros, Quaternion.identity);
+		}
+	}
+}
---
+++ OOP_CA/Assets/Scripts/MapDestroyer.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapDestroyer : MonoBehaviour {
+
+	public Tilemap tilemap;
+
+	public Tile wallTile;
+	public Tile rockTile;
+	public Tile destructableTile;
+
+	public GameObject explosionPrefab;
+
+	public void Explode(Vector2 worldPos)
+	{
+		Vector3Int originCell = tilemap.WorldToCell(worldPos);
+
+		ExplodeCell(originCell);
+
+		if(ExplodeCell(originCell + new Vector3Int(1, 0, 0)))//x
+		{
+			ExplodeCell(originCell + new Vector3Int(2, 0, 0));
+		}
+
+		if(ExplodeCell(originCell + new Vector3Int(0, 1, 0)))//y
+		{
+			ExplodeCell (originCell + new Vector3Int (0, 2, 0));
+		}
+	
+		if(ExplodeCell(originCell + new Vector3Int(-1, 0, 0)))//negative x
+		{
+			ExplodeCell (originCell + new Vector3Int (-2, 0, 0));
+		}
+		if(ExplodeCell(originCell + new Vector3Int(0, -1, 0)))//negative y
+		{
+			ExplodeCell (originCell + new Vector3Int (0, -2, 0));
+		}
+
+	}
+
+	bool ExplodeCell(Vector3Int cell)
+	{
+		Tile tile = tilemap.GetTile<Tile>(cell);
+
+		if (tile == wallTile) 
+		{
+			return false;
+		}
+
+		if (tile == rockTile) 
+		{
+			return false;
+		}
+
+		if (tile == destructableTile) 
+		{
+			tilemap.SetTile(cell, null);
+
+		}
+
+		Vector3 pos = tilemap.GetCellCenterWorld(cell);
+		Instantiate(explosionPrefab, pos, Quaternion.identity);
+
+		return true;
+	}
+
+}
