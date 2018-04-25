d43c36d1ef028e491a777cd7d14e02cd7f5a0440
Mikee <c16363646@mydit.ie>
2018-04-25 15:59:15 +0100

    Bomb Key
    When key space is pressed bomb spawns where ever the player is

--- OOP_CA/Assets/Scripts/CreateBomb.cs
+++ OOP_CA/Assets/Scripts/CreateBomb.cs
@@ -6,15 +6,16 @@ public class CreateBomb : MonoBehaviour
 	public Tilemap tilemap;
 
 	public GameObject bombPrefab;
+	public GameObject player;
 
 		
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		if (Input.GetMouseButtonDown (0)) 
+		if (Input.GetKeyDown(KeyCode.Space)) 
 		{
-			Vector3 worldPos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			Vector3 worldPos = player.transform.position;
 			Vector3Int cell = tilemap.WorldToCell(worldPos);
 			Vector3 cellCenterPros = tilemap.GetCellCenterWorld(cell);
 
