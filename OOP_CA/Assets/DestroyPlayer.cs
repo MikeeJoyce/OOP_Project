e8323b41c08f444ea41403dfc15606df76ff3172
Mikee <c16363646@mydit.ie>
2018-04-26 18:12:18 +0100

    Player Deaths
    Bombs destroys Players

--- OOP_CA/Assets/DestroyPlayer.cs
+++ OOP_CA/Assets/DestroyPlayer.cs
@@ -1,16 +1,1 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DestroyPlayer : MonoBehaviour {
-
-	public GameObject ExplosionPrefab;
-	public GameObject Player;
-
-	void  OnCollisionEnter(Collision ExplosionPrefab) {
-		if(ExplosionPrefab.gameObject.tag == "Player") {
-			Debug.Log ("ded");
-			Destroy(gameObject);
-		}
-	}
-}
+﻿
