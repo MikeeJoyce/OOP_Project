7f99a048af63eab081fb25cf06dc990a18faa7e9
Mikee <c16363646@mydit.ie>
2018-04-27 00:25:04 +0100

    Updated Tilemap
    Changed map

---
+++ OOP_CA/Assets/Scripts/Bomb.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour {
+
+	public float countdown = 2f;
+	float tilesize = 1;
+	public GameObject player1;
+	public GameObject player2;
+
+	void Awake()
+	{
+		player1 = GameObject.Find("Player");
+		player2 = GameObject.Find("Player 2");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		countdown -= Time.deltaTime;
+
+		if (countdown <= 0f) {
+			FindObjectOfType<MapDestroyer>().Explode(transform.position);
+
+			//check if we can kill player 1
+			if (player1 != null && Vector3.Distance (transform.position, player1.transform.position) 
+				< (tilesize * 2.5 )) {
+
+				//if the player's x pos is close to ours we kill him
+				if (player1.transform.position.x < (transform.position.x + tilesize / 2) &&
+				   player1.transform.position.x > (transform.position.x - tilesize / 2)) {
+					Destroy (player1);
+				}
+
+				//if the player's y pos is close to ours we kill him
+				if (player1.transform.position.y < (transform.position.y + tilesize / 2) &&
+					player1.transform.position.y > (transform.position.y - tilesize / 2)) {
+					Destroy (player1);
+				}
+			}
+
+
+
+			//check if we can kill player 2
+			if (player2 != null && Vector3.Distance (transform.position, player2.transform.position) 
+				< (tilesize * 2.5 )) {
+
+				//if the player's x pos is close to ours we kill him
+				if (player2.transform.position.x < (transform.position.x + tilesize / 2) &&
+					player2.transform.position.x > (transform.position.x - tilesize / 2)) {
+					Destroy (player2);
+				}
+
+				//if the player's y pos is close to ours we kill him
+				if (player2.transform.position.y < (transform.position.y + tilesize / 2) &&
+					player2.transform.position.y > (transform.position.y - tilesize / 2)) {
+					Destroy (player2);
+				}
+			}
+
+			Destroy (gameObject);
+		}
+
+
+
+	}
+}
