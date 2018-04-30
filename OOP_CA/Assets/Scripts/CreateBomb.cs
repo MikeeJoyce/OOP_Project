4e2f1dce4b00b087bb6aa09afa1b0202a2a9caf6
Mikee <c16363646@mydit.ie>
2018-04-30 23:17:52 +0100

    Menu
    Made background

--- OOP_CA/Assets/Scripts/CreateBomb.cs
+++ OOP_CA/Assets/Scripts/CreateBomb.cs
@@ -13,7 +13,7 @@ public class CreateBomb : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		if (Input.GetKeyDown(KeyCode.F)) 
+		if (Input.GetKeyDown(KeyCode.Tab)) 
 		{
 			Vector3 worldPos = player.transform.position;
 			Vector3Int cell = tilemap.WorldToCell(worldPos);
